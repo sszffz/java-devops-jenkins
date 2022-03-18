6720bd9ff2943443e3438cdbdc1bb021a7b86f8c
sszffz <yunfenglee14@gmail.com>
2022-03-17 22:48:36 -0500

    Init

---
+++ src/main/java/com/ffz/devops/JavaDevopsJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.ffz.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevopsJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDevopsJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ffz/devops/JavaDevopsJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffz.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevopsJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
